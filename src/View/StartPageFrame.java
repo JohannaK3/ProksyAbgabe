5d8003fd7413ba9492c566de9d6bc73314526b91
johannakrickow <johanna@krickow.net>
2023-06-13 17:40:49 +0200

    GridBagLayout begonnen

--- src/View/StartPageFrame.java
+++ src/View/StartPageFrame.java
@@ -2,11 +2,13 @@ package View;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 import java.time.LocalDate;
 
-public class StartPageFrame extends JFrame{
+public class StartPageFrame extends JFrame  {//implements ActionListener
 
     private final int centerJLabel = SwingConstants.CENTER;
+    private final Insets basicInsets = new Insets(5, 5, 5, 5);
 
     private Container backgroundContainer;
     private Font menuFont;
@@ -19,8 +21,8 @@ public class StartPageFrame extends JFrame{
     private JPanel mealTablePanel;
     private JPanel bottomPanel;
 
-    private JComboBox<LocalDate> chooseDate; //TODO: instantiate
-    private LocalDate[] chooseDateArray; //TODO: get future dates from Controller/Model
+    //private JComboBox<LocalDate> chooseDate;
+    //private LocalDate[] chooseDateArray; //TODO: get future dates from Controller/Model
 
     private JButton showHistory;
     private JButton showNutrients;
@@ -40,6 +42,7 @@ public class StartPageFrame extends JFrame{
         setExtendedState(MAXIMIZED_BOTH); //TODO: really the way to maximize to full screen?
         setResizable(true);
         setLocationRelativeTo(null);
+        //getContentPane().setBackground(Color.red);
     }
 
     private void setFont() {
@@ -50,20 +53,37 @@ public class StartPageFrame extends JFrame{
         bottomFont = new Font("Arial", Font.PLAIN, 10);
     }
 
+    //private void setFontOf(Font font) {
+
+   // }
+
     public StartPageFrame() {
+
+        backgroundContainer = getContentPane();
 
         this.setTitle("MENSA FOOD TRACKER");
         this.initialize();
-
-        backgroundContainer = getContentPane();
-        backgroundContainer.setLayout(new GridLayout(6, 1, 1, 0));
-
         this.setFont();
+        this.getContentPane().setBackground(new Color(0xf5efdc));
 
-        menuPanel = new JPanel(new GridLayout(1, 3));
-        mealTablePanel = new JPanel(new GridLayout(10, 3));
-        bottomPanel = new JPanel(new GridLayout(1, 2));
+        backgroundContainer.setLayout(new GridBagLayout());
 
+        GridBagConstraints backgroundConstraints = new GridBagConstraints();
+        backgroundConstraints.insets = basicInsets;
+
+        menuPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints menuPanelConstraints = new GridBagConstraints();
+        menuPanelConstraints.insets = basicInsets;
+
+        mealTablePanel = new JPanel(new GridBagLayout());
+        GridBagConstraints meaLTablePanelConstraints = new GridBagConstraints();
+        meaLTablePanelConstraints.insets = basicInsets;
+
+        bottomPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints bottomPanelConstraints = new GridBagConstraints();
+        bottomPanelConstraints.insets = basicInsets;
+
+        //chooseDate = new JComboBox<>(chooseDateArray);
         showHistory = new JButton("Essenshistorie");
         showNutrients = new JButton("Kumulierte Nährwertangabe");
 
@@ -74,5 +94,15 @@ public class StartPageFrame extends JFrame{
         lastUpdate = new JLabel("Letztes Update: ", SwingConstants.LEFT);
         dateAndTimeOfLastUpdate = new JLabel("", SwingConstants.RIGHT); //TODO: get last update from controller
         author = new JLabel("Autor: Johanna Krickow", centerJLabel);
+
+        //menuPanel.add(chooseDate, 1, 1);
+        menuPanel.add(showHistory);
+        menuPanel.add(showNutrients);
+        backgroundContainer.add(menuPanel);
+
+        mealTablePanel.add(todaysMenu);
+        mealTablePanel.add(mealName);
+        backgroundContainer.add(mealTablePanel);
+
     }
 }
